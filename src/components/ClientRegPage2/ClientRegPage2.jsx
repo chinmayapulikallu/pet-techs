1b15c818e58225b2606b8953c17163a39348ad6c
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-06-19 13:49:44 -0500

    base setup

---
+++ src/components/ClientRegPage2/ClientRegPage2.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ClientRegPage2 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Client Registration Page 2</h1>
+            </div>
+        )
+    }
+}
+export default ClientRegPage2;

9a893384cff383185d7c9bf9fe92906da982d478
ThyChristiansen <thyvu0411@gmail.com>
2020-06-19 14:27:41 -0500

    set up database

--- database.sql
+++ database.sql
@@ -8,3 +8,142 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "user" (
+  id SERIAL PRIMARY KEY,
+  user_name varchar (80) NOT NULL,
+  user_email varchar (80) NOT NULL,
+  password varchar (20)NOT NULL,
+  phone_number int NOT NULL,
+  hear_about text NOT NULL,
+  user_type int NOT NULL
+);
+
+
+CREATE TABLE client (
+  id SERIAL PRIMARY KEY,
+  "user_id" INT NOT NULL
+        REFERENCES "user" (id)
+        ON DELETE CASCADE ,
+  client_name varchar (80) NOT NULL,
+  home_address_house varchar NOT NULL,
+  apt_suite varchar (80),
+  city varchar (100) NOT NULL,
+  "state" varchar (20) NOT NULL,
+  zip_code int NOT NULL,
+  profile_img varchar (1000),
+  about_client text NOT NULL,
+  about_home text NOT NULL,
+  contact_name_1 varchar (80),
+  contact_phone_1 int,
+  contact_email_1 varchar (100),
+  vet_clinic varchar (250),
+  clinic_address varchar (250),
+  clinic_phone int ,
+  transport boolean 
+);
+
+CREATE TABLE vet_tech (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL
+        REFERENCES "user" (id)
+        ON DELETE CASCADE ,
+  vet_name varchar (80) NOT NULL,
+  home_address_house varchar (100) NOT NULL,
+  apt_suite varchar (80),
+  city varchar (80) NOT NULL,
+  state varchar (80) NOT NULL,
+  zip_code int,
+  profile_img varchar (1000),
+  sleep_over boolean DEFAULT FALSE,
+  boarding boolean DEFAULT FALSE,
+  dropin_care boolean DEFAULT FALSE,
+  hospice boolean DEFAULT FALSE,
+  about_vet text NOT NULL,
+  --  dogs boolean ,
+  --  cats boolean,
+  --  other boolean,
+  type_pet_provide varchar (50) NOT NULL,
+  vet_available boolean NOT NULL,
+  zero_two boolean NOT NULL,
+  two_four boolean NOT NULL,
+  four_eight boolean NOT NULL,
+  na boolean NOT NULL,
+--  small boolean
+--  medium boolean
+--  large boolean
+--  giant boolean
+  pet_size varchar (30) NOT NULL,
+  pet_younger_than_one boolean,
+  pet_more_than_one_family boolean,
+  equipment_list text ,
+  
+  experience int NOT NULL ,
+  education text,
+  certifications text, 
+  current_job_title text,
+  expertise varchar ,
+  CPR_first_air boolean NOT NULL,
+  oral_medication boolean NOT NULL,
+  injectable_medicaiton boolean NOT NULL,
+  exp_older_pet boolean NOT NULL,
+  exp_special_pet boolean NOT NULL,
+  daily_exercise boolean NOT NULL,
+  pet_longer_than_a_week boolean NOT NULL,
+  diabetic_insulin_care boolean NOT NULL
+);
+
+CREATE TABLE pet (
+  id SERIAL PRIMARY KEY,
+  client_id INT NOT NULL
+            REFERENCES client (id)
+            ON DELETE CASCADE ,
+  pet_type varchar (50) NOT NULL,
+  pet_name varchar (20) NOT NULL,
+  weight int NOT NULL,
+  age int NOT NULL,
+  sex varchar NOT NULL,
+  breed varchar (50) NOT NULL,
+  pet_img varchar (1000),
+  pet_bio varchar NOT NULL,
+  food_brand varchar (250) NOT NULL,
+  feeding_per_day int NOT NULL,
+  amount_per_meal varchar (100) NOT NULL,
+  other_food varchar (500),
+  pet_behavior text NOT NULL,
+  care_equipment text
+--  other text
+);
+
+CREATE TABLE medication (
+  id SERIAL PRIMARY KEY,
+  pet_id INT NOT NULL
+         REFERENCES pet (id)
+         ON DELETE CASCADE ,
+  medication_name varchar (500) NOT NULL,
+  dosage varchar (500) NOT NULL,
+  dosage_time time NOT NULL
+);
+
+CREATE TABLE pet_picture (
+  id SERIAL PRIMARY KEY,
+  pet_id INT NOT NULL
+         REFERENCES pet (id)
+         ON DELETE CASCADE ,
+  pet_img varchar (1000)
+);
+
+CREATE TABLE client_request (
+  id SERIAL PRIMARY KEY,
+  pet_id INT NOT NULL
+         REFERENCES pet (id)
+         ON DELETE CASCADE ,
+  vet_id INT NOT NULL
+         REFERENCES vet_tech (id)
+         ON DELETE CASCADE ,
+  start_date_time date,
+  end_date_time date,
+  add_info text,
+  resquest_status int 
+);
+

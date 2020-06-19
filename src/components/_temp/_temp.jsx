44d1ebfe6a17a6ee8c101290fedf5759825395d6
ThyChristiansen <thyvu0411@gmail.com>
2020-06-19 11:44:14 -0500

    template component

---
+++ src/components/_temp/_temp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NAME extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+export default NAME;

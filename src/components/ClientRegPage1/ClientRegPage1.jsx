554e97fde04c2a46ca9894bdab291dbd8da5d015
Heather Stoffels <Umdstarlet@gmail.com>
2020-06-19 13:39:32 -0500

    added components to App

---
+++ src/components/ClientRegPage1/ClientRegPage1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ClientRegPage1 extends Component {
+
+    
+  render() {
+    return (
+      <div>
+        <h2>FILL INFO</h2>
+      </div>
+    )
+  }
+}
+export default ClientRegPage1;

1e45117485deb6645ac4673b9b5d5fabb315176c
Mike Cossalter <mikecossalter@yahoo.com>
2020-06-20 14:53:43 -0500

    working MUI form

--- src/components/VTPage2/VTPage2.jsx
+++ src/components/VTPage2/VTPage2.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { withRouter } from "react-router"
-import Grid from '@material-ui/core/Grid'
-import FormControl from "@material-ui/core/FormControl"
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
-import Radio from "@material-ui/core/Radio"
+import FormControl from '@material-ui/core/FormControl'
+import FormLabel from "@material-ui/core/FormLabel"
+import { FormControlLabel } from '@material-ui/core';
 import FormGroup from "@material-ui/core/FormGroup"
-
+import Checkbox from "@material-ui/core/Checkbox"
+import FormHelperText from "@material-ui/core/FormHelperText"
+import Radio from "@material-ui/core/Radio"
+import TextField from "@material-ui/core/TextField"
+import Grid from "@material-ui/core/Grid"
 
 
 
@@ -38,12 +40,111 @@ class VTPage2 extends Component {
     }
 
     render() {
+       
         return (
+            
             <div>
                 <div>
                     <h2>What are your service preferences</h2>
                 </div>
                 <div>
+                    <FormControl component="fieldset">
+                        <FormLabel component="label">What animals will you provide services for?</FormLabel>
+                        <FormGroup row="true">
+                            <FormControlLabel
+                            control={<Checkbox  name="dog" />}
+                            label="Dogs"
+                            />
+                            <FormControlLabel
+                            control={<Checkbox  name="cat" />}
+                            label="Cats"
+                            />
+                            <FormControlLabel
+                            control={<Checkbox  name="other" />}
+                            label="Others"
+                            />
+                        </FormGroup>
+                        <FormHelperText>Please choose all that apply</FormHelperText>
+                        <FormLabel component="label">Are you home full time Monday-Friday?</FormLabel>
+                        <FormGroup row="true">
+                            <FormControlLabel
+                                control={<Checkbox name="availYes" />}
+                                label="Yes"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox name="availNo" />}
+                                label="No"
+                            />
+                        </FormGroup>
+                        <FormLabel component="label">How often can you take the animal(s) outside?</FormLabel>
+                        <FormGroup row="true">
+                            <FormControlLabel
+                                control={<Checkbox name="hourChoice1" />}
+                                label="0-2 Hours"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox name="hourChoice2" />}
+                                label="2-4 Hours"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox name="hourChoice3" />}
+                                label="4-8 Hours"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox name="hourChoice4" />}
+                                label="N/A"
+                            />
+                        </FormGroup>
+                        <FormLabel component="label">What size animals are you comfortable hosting?</FormLabel>
+                        <FormGroup row="true">
+                            <FormControlLabel
+                                control={<Checkbox name="sizeSm" />}
+                                label="Small"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox name="sizeMd" />}
+                                label="Medium"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox name="sizeLg" />}
+                                label="Large"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox name="sizeGt" />}
+                                label="Giant"
+                            />
+                        </FormGroup>
+                        <FormLabel component="label">Will you host animals younger than 1 years old?</FormLabel>
+                        <FormGroup row="true">
+                            <FormControlLabel
+                                control={<Radio name="youngYes" />}
+                                label="Yes"
+                            />
+                            <FormControlLabel
+                                control={<Radio name="youngNo" />}
+                                label="No"
+                            />
+                        </FormGroup>
+                        <FormLabel component="label">Will you host animals from multiple families at once?</FormLabel>
+                        <FormGroup row="true">
+                            <FormControlLabel
+                                control={<Radio name="multiYes" />}
+                                label="Yes"
+                            />
+                            <FormControlLabel
+                                control={<Radio name="multiNo" />}
+                                label="No"
+                            />
+                        </FormGroup>
+                        <Grid>
+                        <TextField type="text" id="outlined-basic" label="Optional" variant="outlined"
+                            color="secondary"
+                        />
+                        </Grid>
+                    </FormControl>
+                   
+                </div>
+                {/* <div>
                     <h4>What animals will you provide services for?</h4>
                     <input type="checkbox" name="dogCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "dogs")} /> Dogs
                      <input type="checkbox" name="dogCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "cats")} /> Cats
@@ -94,7 +195,7 @@ class VTPage2 extends Component {
                 <div>
                     <button onClick={this.handleBackClick}>Back</button>
                     <button onClick={this.handleContClick}>Continue</button>
-                </div>
+                </div> */}
 
             </div>
 

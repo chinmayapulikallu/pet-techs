b36339027b4734f3048b300b7306f294340f8c2a
Mike Cossalter <mikecossalter@yahoo.com>
2020-06-22 09:12:30 -0500

    merge with master

--- src/components/VTPage2/VTPage2.jsx
+++ src/components/VTPage2/VTPage2.jsx
@@ -56,6 +56,7 @@ class VTPage2 extends Component {
     }
 
     //THIS WILL TAKE THE VET TECH TO THE 3RD VET TECH REGISTRATION PAGE
+    //AND DISPATCH THE STATE
     handleContClick = () => {
         console.log("CLICKED CONTINUE");
         // this.props.dispatch({
@@ -79,7 +80,6 @@ class VTPage2 extends Component {
                         <FormGroup row="true">
                             <FormControlLabel
                                 control={<Checkbox name="dog" />}
-                                
                                 value={this.state.dog}
                                 onChange={(event) => this.handleChange(event, "dogs")}
                                 label="Dogs"
@@ -230,59 +230,7 @@ class VTPage2 extends Component {
                     </FormControl>
                 </Grid>
 
-                {/* <div>
-                    <h4>What animals will you provide services for?</h4>
-                    <input type="checkbox" name="dogCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "dogs")} /> Dogs
-                     <input type="checkbox" name="dogCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "cats")} /> Cats
-                     <input type="checkbox" name="dogCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "other")} /> Other
-           </div>
-                <div>
-                    <h4>Are you home full time Monday-Friday?</h4>
-                    <input type="radio" name="yesRadio" defaultChecked={false} onChange={(event) => this.handleChange(event, "yes")} /> Yes
-                    <input type="radio" name="noRadio" defaultChecked={false} onChange={(event) => this.handleChange(event, "no")} /> No
-           </div>
-                <div>
-                    <h4>How frequently can you take dogs outside?</h4>
-                    <input type="checkbox" name="hrCheck1" defaultChecked={false} onChange={(event) => this.handleChange(event, "0-2")} /> 0-2 hrs
-                    <input type="checkbox" name="hrCheck2" defaultChecked={false} onChange={(event) => this.handleChange(event, "2-4")} /> 2-4 hrs
-                    <input type="checkbox" name="hrCheck3" defaultChecked={false} onChange={(event) => this.handleChange(event, "4-8")} /> 4-8 hrs
-                    <input type="checkbox" name="hrCheck4" defaultChecked={false} onChange={(event) => this.handleChange(event, "N/A")} /> N/A
-                </div>
-                <div>
-                    <h4>What size animals are you comfortable hosting?</h4>
-                    <input type="checkbox" name="smCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "small")} /> Small
-                    <input type="checkbox" name="medCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "medium")} /> Medium
-                    <input type="checkbox" name="lgCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "large")} /> Large
-                    <input type="checkbox" name="gtCheck" defaultChecked={false} onChange={(event) => this.handleChange(event, "giant")} /> Giant
-                </div>
-                <div>
-                    <h4>Will you host animals younger than 1 year old?</h4>
-                    <input type="radio" name="yesAgeRadio" defaultChecked={false} onChange={(event) => this.handleChange(event, "yesAge")} /> Yes
-                    <input type="radio" name="noAgeRadio" defaultChecked={false} onChange={(event) => this.handleChange(event, "noAge")} /> No
-           </div>
-                <div>
-                    <h4>Will you host animals from more than one family at a time?</h4>
-                    <input type="radio" name="multiYesRadio" defaultChecked={false} onChange={(event) => this.handleChange(event, "yesMulti")} /> Yes
-                    <input type="radio" name="multiNoRadio" defaultChecked={false} onChange={(event) => this.handleChange(event, "noMulti")} /> No
-           </div>
-                <div>
-                    <h4>Please provide a list of equipment that will be used when caring for pets:</h4>
-                    <textarea
-                        className="equipment"
-                        placeholder="Optional..."
-                        rows={5}
-                        rowsmax={5}
-                        cols={100}
-                        onChange={(event) => this.handleChange(event, "equipment")}
-                    />
-
-                </div>
                
-                <div>
-                    <button onClick={this.handleBackClick}>Back</button>
-                    <button onClick={this.handleContClick}>Continue</button>
-                </div> */}
-
             </div>
 
         )
@@ -309,7 +257,6 @@ const mapStateToProps = (state) => ({
     },
     errors: state.errors,
 });
-// export default
-//     connect(mapStateToProps)(VTPage2);
 
-export default withRouter(connect(mapStateToProps)(withStyles(styles, { withTheme: true })(VTPage2)));
+
+export default withRouter(connect(mapStateToProps)(withStyles(styles)(VTPage2)));

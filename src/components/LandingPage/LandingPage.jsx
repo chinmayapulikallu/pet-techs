21f9dd80702949baac8c8fd70e58fbaa425c5bce
Mike Cossalter <mikecossalter@yahoo.com>
2020-06-22 15:29:32 -0500

    working on text overlays

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -5,6 +5,10 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid"
 import Button from "@material-ui/core/Button"
 import Typeography from "@material-ui/core/Typography"
+import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import Typography from '@material-ui/core/Typography';
+
 
 
 const styles = theme => ({
@@ -13,13 +17,33 @@ const styles = theme => ({
     },
     description: {
         textAlign: "center",
+        fontFamily: 'Quicksand',
         alignItems: 'center',
         justifyContent: "center",
         alignItems: "center",
         marginRight: 400,
         marginLeft: 400,
-        color: '#195C60'
-    }
+        color: '#195C60',
+        paddingTop: 20,
+        paddingBottom: 20
+    },
+    services: {
+        textAlign: "center",
+        fontFamily: 'Quicksand',
+        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 400,
+        marginLeft: 400,
+        paddingTop: "50px"
+    },
+    cards: {
+        fontFamily: 'Quicksand',
+        margin: "25px",
+        textAlign: 'center',
+        justifyContent: "center"
+    },
+
 
 })
 
@@ -33,14 +57,74 @@ class LandingPage extends Component {
                     <img className={classes.landingPg} src='../images/homeDog.png' />
                 </Grid>
                 <div>
-                    <br />
-                    <Grid>
-                        <Grid container spacing={0} alignItems="center" justify="center">
+                    <Grid >
+                        <Grid container spacing={3} >
                             <Typeography className={classes.description} variant="h5">You love your pets. We love that you love your pets. Pet Techs is here to make sure you have qualified indviduals to help you take care of them.</Typeography>
                         </Grid>
                     </Grid>
                 </div>
+                <div>
+                    <Typeography className={classes.services} variant="h6"><b>Our Services</b></Typeography>
+                    <Grid container direction="row" className={classes.cards} spacing={2}>
+                        <Grid item xs={2}>
+                            <Card>
+                                <CardContent>
+                                    <div>
+                                        <img className={classes.image} src="/images/sleepover-icon.png" alt="sleepoverIcon" height="75" width="75" />
+                                        <Typeography variant="h6"><b>Pet Sleepovers</b></Typeography>
+                                        <Typeography variant="subtitle1">We love slumber parties! Providers watch your pet in the comfort of your own home. Jo need for you to pack up and transport!</Typeography>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Card>
+                                <CardContent>
+                                    <div>
+                                        <img className={classes.image} src="/images/boarding-icon.png" alt="boardingIcon" height="75" width="75" />
+                                        <Typeography variant="h6"><b>Pet Boarding</b></Typeography>
+                                        <Typeography variant="subtitle1">Our care providers have cozy homes for your four legged friends. Have them watched in a providers home with pet boarding.</Typeography>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Card>
+                                <CardContent>
+                                    <div>
+                                        <img className={classes.image} src="/images/drop-in-care-icon.png" alt="dropInCare" height="75" width="75" />
+                                        <Typeography variant="h6"><b>Drop in Care</b></Typeography>
+                                        <Typeography variant="subtitle1">Service providers drop by your home to help provide insulin injections, basic medical needs, or routing care.</Typeography>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Card>
+                                <CardContent>
+                                    <div>
+                                        <img className={classes.image} src="/images/hospice-icon.png" alt="hospiceIcon" height="75" width="75" />
+                                        <Typeography variant="h6"><b>Hospice Care</b></Typeography>
+                                        <Typeography variant="subtitle1">Hospice service centers around providers coming to your home, helping with general hospice comfort, maintenance and care.</Typeography>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </div>
+
+                <div className={classes.root}>
+                    <Grid container spacing={3}>
+
+                        <img className={classes.landingPg} src='../images/homeCat.png' />
+                       
+                       
+                    </Grid>
+                </div>
             </div>
+
+
+
 
         )
     }

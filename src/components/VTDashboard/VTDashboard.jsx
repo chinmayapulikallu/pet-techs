3a2357f7d54570ab8a2c12b027941c2cbd730494
Heather Stoffels <Umdstarlet@gmail.com>
2020-06-22 14:42:58 -0500

    added mapstatetoprops and connect.

--- src/components/VTDashboard/VTDashboard.jsx
+++ src/components/VTDashboard/VTDashboard.jsx
@@ -14,7 +14,7 @@ class VTDashboard extends Component {
   componentDidMount = () => {
     // get dashboard info for Vet Tech, get request
   };
-// not sure if these will be the exact paths, or types, placeholders for now. 
+  // not sure if these will be the exact paths, or types, placeholders for now.
   detailsButton = (event) => {
     this.props.dispatch({
       type: "GET_VT_SERVICE_REQUEST_BY_ID",
@@ -22,7 +22,7 @@ class VTDashboard extends Component {
     });
     this.props.history.push("/vtservicerequest");
   };
- // not sure if these will be the exact paths, or types, placeholders for now. 
+  // not sure if these will be the exact paths, or types, placeholders for now.
   viewButton = (event) => {
     this.props.dispatch({
       type: "GET_CARE_PLAN__BY_ID",
@@ -32,79 +32,80 @@ class VTDashboard extends Component {
   };
   render() {
     return (
-        <div>
-      <Container>
-        <h1>Photo will go here</h1>
-        <h1>Photo will go here</h1>
-        <h2>Pending Requests</h2>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Date</TableCell>
-                <TableCell>Client Name</TableCell>
-                <TableCell>Details</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Date</TableCell>
-                <TableCell>Client Name</TableCell>
-                <TableCell>
-                <Button
-                  size="small"
-                  variant="contained"
-                  // will need to add a value, (id) for event to capture.
-                  onClick={this.detailsButton}
-                >
-                  Details
-                </Button>
-              </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Container>
-       <Container>
-       <h2>Upcoming Commitments</h2>
-       <TableContainer component={Paper}>
-         <Table>
-           <TableHead>
-             <TableRow>
-               <TableCell>Pet Name</TableCell>
-               <TableCell>Date</TableCell>
-               <TableCell>Species Name</TableCell>
-               <TableCell>Action</TableCell>
-             </TableRow>
-           </TableHead>
-           <TableBody>
-             <TableRow>
-               <TableCell>Name</TableCell>
-               <TableCell>Date</TableCell>
-               <TableCell>Species</TableCell>
-               <TableCell>
-               <Button
-                 size="small"
-                 variant="contained"
-                 // will need to add a value, (id) for event to capture.
-                 onClick={this.viewButton}
-               >
-                 View
-               </Button>
-             </TableCell>
-             </TableRow>
-           </TableBody>
-         </Table>
-       </TableContainer>
-     </Container>
-     </div>
+      <div>
+        <Container>
+          <h1>Photo will go here</h1>
+          <h1>Photo will go here</h1>
+          <h2>Pending Requests</h2>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Client Name</TableCell>
+                  <TableCell>Details</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Client Name</TableCell>
+                  <TableCell>
+                    <Button
+                      size="small"
+                      variant="contained"
+                      // will need to add a value, (id) for event to capture.
+                      onClick={this.detailsButton}
+                    >
+                      Details
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
+        <Container>
+          <h2>Upcoming Commitments</h2>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Pet Name</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Species Name</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Species</TableCell>
+                  <TableCell>
+                    <Button
+                      size="small"
+                      variant="contained"
+                      // will need to add a value, (id) for event to capture.
+                      onClick={this.viewButton}
+                    >
+                      View
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
+      </div>
     );
   }
 }
 const mapStateToProps = (state) => ({
- // not sure about the reducer names yet
-  });
+  // not sure about the reducer names yet
+  clientInfo: state.clientInfo,
+});
 
 export default connect(mapStateToProps)(VTDashboard);

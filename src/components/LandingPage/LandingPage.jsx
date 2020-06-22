06eade39574b130d36347cecbc98b426db0d5cef
Mike Cossalter <mikecossalter@yahoo.com>
2020-06-22 11:45:01 -0500

    fixed color issue on text

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -5,12 +5,9 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid"
 import Button from "@material-ui/core/Button"
 import Typeography from "@material-ui/core/Typography"
-import { green } from '@material-ui/core/colors';
 
 
 const styles = theme => ({
-
-    
     landingPg: {
         borderRadius: 0,
     },
@@ -18,17 +15,16 @@ const styles = theme => ({
         textAlign: "center",
         alignItems: 'center',
         justifyContent: "center",
-        alignItems: "center", 
-        marginRight: 375,
-        marginLeft: 375
+        alignItems: "center",
+        marginRight: 400,
+        marginLeft: 400,
+        color: '#195C60'
     }
 
 })
 
 
 class LandingPage extends Component {
-
-
     render() {
         const { classes } = this.props
         return (
@@ -39,14 +35,8 @@ class LandingPage extends Component {
                 <div>
                     <br />
                     <Grid>
-                        <Grid
-                            container
-                            spacing={0}
-                            alignItems="center"
-                            justify="center"
-                            
-                        >
-                            <Typeography className={classes.description} variant="h6">You love your pets. We love that you love your pets. Pet Techs is here to make sure you have qualified indviduals to help you take care of them.</Typeography>
+                        <Grid container spacing={0} alignItems="center" justify="center">
+                            <Typeography className={classes.description} variant="h5">You love your pets. We love that you love your pets. Pet Techs is here to make sure you have qualified indviduals to help you take care of them.</Typeography>
                         </Grid>
                     </Grid>
                 </div>
@@ -55,6 +45,6 @@ class LandingPage extends Component {
         )
     }
 }
-// export default LandingPage;
+// export default LandingPage
 
 export default (withStyles(styles)(LandingPage));

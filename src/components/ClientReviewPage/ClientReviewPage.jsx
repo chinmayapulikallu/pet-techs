8cfacfeb4fc872b94fa3ba003164061e466c05bd
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-06-22 11:15:13 -0500

    added another pet functionality and created template for client review page

---
+++ src/components/ClientReviewPage/ClientReviewPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ClientReviewPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>CLient Review Page</h1>
+            </div>
+        )
+    }
+}
+export default ClientReviewPage;

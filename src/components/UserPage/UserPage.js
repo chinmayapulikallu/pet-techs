9e4fc78ba2592d834031c6f21a79af959ddd5e35
ThyChristiansen <thyvu0411@gmail.com>
2020-06-23 11:05:01 -0500

    pass the passport

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -10,6 +10,11 @@ const UserPage = (props) => (
     <h1 id="welcome">
       Welcome, { props.user.username }!
     </h1>
+    <p>sdf</p>
+    <p>sdf</p>
+    <p>sdf</p>
+    <p>sdf</p>
+
     <p>Your ID is: {props.user.id}</p>
     <LogOutButton className="log-in" />
   </div>
@@ -18,8 +23,8 @@ const UserPage = (props) => (
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({user}) => ({ user });
-const mapStateToProps = state => ({
-  user: state.user,
+const mapStateToProps = reduxState => ({
+  user: reduxState.user,
 });
 
 // this allows us to use <App /> in index.js

7411324843697b62f412ebb17e46372624bdb89e
Mike Cossalter <mikecossalter@yahoo.com>
2020-06-24 13:42:12 -0500

    working client service request

--- src/components/ClientServiceRequest/ClientServiceRequest.jsx
+++ src/components/ClientServiceRequest/ClientServiceRequest.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import React, {useState} from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
 import FormControl from "@material-ui/core/FormControl";
@@ -27,8 +26,11 @@ const styles = theme => ({
         textAlign: 'center',
     },
     boxes: {
-        marginTop: 10,
-        marginBottom: 10
+        marginLeft: 20,
+        marginTop: 15
+    },
+    datePicker: {
+        // marginTop: 50
     },
     button: {
         margin: "20px 30px 20px 30px",
@@ -36,6 +38,9 @@ const styles = theme => ({
         width: 180,
         borderRadius: 12,
     },
+    info: {
+        marginTop: 20
+    }
 })
 
 class ClientServiceRequest extends Component {
@@ -46,86 +51,123 @@ class ClientServiceRequest extends Component {
     }
 
 
-    handleChange = date => {
-        this.setState({
-            startDate: date
-        })
+    handleChange = (event, property) => {
+        switch (property) {
+            case "start_date":
+                return this.setState({
+                    [property]: event.target.value
+                });
+            case "end_date":
+                return this.setState({
+                    [property]: event.target.value
+                })
+        }
     }
 
-   
+
     render() {
         const { classes } = this.props
-            
-            
-           
+        const startDate = this.state
+        const endDate = this.state
+
+
         return (
             <div className={classes.root}>
-               
+
                 <div>
                     <Grid className={classes.title}>
                         <FormControl>
                             <Typography className={classes.title} variant="h3">Request Service</Typography>
-                          <div>
-                                <FormControl variant="outlined" className={classes.formControl}>
+                            <div>
+                                <Typography variant="h6">Please select service:</Typography>
+                                <br />
+                                <FormControl variant="outlined">
                                     <InputLabel id="demo-simple-select-outlined-label">Pet Type</InputLabel>
                                     <Select
                                         labelId="demo-simple-select-outlined-label"
                                         id="demo-simple-select-outlined"
                                         value={0}
                                         color="secondary"
-                                        label="Pet Type"
+                                        label="Service"
                                     >
-                                        <MenuItem value="0">Select</MenuItem>
-                                        <MenuItem value="Dog">Dog</MenuItem>
-                                        <MenuItem value="Cat">Cat</MenuItem>
-                                        <MenuItem value="Other">Other</MenuItem>
+                                        <MenuItem value="0">Pet Sleepover</MenuItem>
+                                        <MenuItem value="Dog">Pet Boarding</MenuItem>
+                                        <MenuItem value="Cat">Drop-In</MenuItem>
+                                        <MenuItem value="Other">Hospice</MenuItem>
                                     </Select>
                                 </FormControl>
-                          </div>
+                            </div>
+                            <br />
+                            <Typography variant="h6">Please select the dates requesting for service:</Typography>
+                            <br />
+                            <div >
+                               
 
-                            <div className={classes.boxes}>
-                                <TextField id="outlined-basic"
-                                    selected={startDate}
-                                    onChange={date => setStartDate(date)}
-                                    selectsStart
-                                    label="Start Date"
-                                    variant="outlined"
-                                    name="start_date"
-                                    color="secondary"
-                                />
-                                <TextField id="outlined-basic"
-                                    selected={endDate}
-                                    onChange={date => endDate(date)}
-                                    selectsEnd
-                                    label="End Date"
-                                    variant="outlined"
-                                    name="end_date"
-                                    color="secondary"
-                                />
                                 <div>
-                                    <DatePicker
-                                        selected={startDate}
-                                        onChange={date => startDate(date)}
+
+                                    <DatePicker className={classes.datePicker}
+                                        selected={this.state.startDate}
+                                        onChange={(event) => this.handleChange(event, "start_date")}
                                         selectsStart
-                                        startDate={startDate}
-                                        endDate={endDate}
+                                    //startDate={startDate}
+                                    //endDate={endDate}
                                     />
                                     <DatePicker
-                                        selected={endDate}
-                                        onChange={date => setEndDate(date)}
+                                        selected={this.state.endDate}
+                                        onChange={(event) => this.handleChange(event, "end_date")}
                                         selectsEnd
-                                        startDate={startDate}
-                                        endDate={endDate}
-                                        minDate={startDate}
-                                        />
+                                    // startDate={startDate}
+                                    // endDate={endDate}
+                                    // minDate={startDate}
+                                    />
+                                    <TextField className={classes.boxes} id="outlined-basic"
+                                        label="Start Date"
+                                        variant="outlined"
+                                        name="start_date"
+                                        color="secondary"
+                                    />
+                                    <TextField className={classes.boxes} id="outlined-basic"
+                                        label="End Date"
+                                        variant="outlined"
+                                        name="end_date"
+                                        color="secondary"
+                                    />
                                 </div>
+                                <div>
+                                    <TextField className={classes.info}
+                                        fullWidth
+                                        multiline
+                                        rows={10}
+                                        type="text"
+                                        id="outlined-basic"
+                                        label="info"
+                                        variant="outlined"
+                                        color="secondary"
+                                    />
+                                </div>
+                                <div>
+                                    <TextField className={classes.info}
+                                        fullWidth
+                                        multiline
+                                        type="text"
+                                        id="outlined-basic"
+                                        label="Any additional information?"
+                                        variant="outlined"
+                                        color="secondary"
+                                    />
+                                </div>
+                                <div>
+                                    <Button className={classes.button} onClick={this.handleJoinClick} variant="contained" color="primary" >Cancel</Button>
+                                    <Button className={classes.button} onClick={this.handSignInClick} variant="contained" color="primary" >Send Request</Button>
+                                </div>
+
                             </div>
-                          
-                           
+
+
                         </FormControl>
                     </Grid>
                 </div>
-               
+
             </div>
         );
     }

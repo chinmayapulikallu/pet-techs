8bfe8056daa6d897092d2bade174aa135d60a8cc
Heather Stoffels <Umdstarlet@gmail.com>
2020-06-24 14:48:23 -0500

    get route for pet info, pet saga and reducer.

--- src/redux/reducers/petInfoReducer.js
+++ src/redux/reducers/petInfoReducer.js
@@ -3,6 +3,8 @@ const petInfoReducer = (state = [], action) => {
     switch (action.type) {
         case 'SET_PET':
             return action.payload;
+        case 'GET_PET_DATA_SUCCESSFUL':
+            return action.payload; 
         default:
             return state;
     }
--- src/redux/sagas/clientInfoSaga.js
+++ src/redux/sagas/clientInfoSaga.js
@@ -1,23 +1,22 @@
-import { put, takeLatest } from 'redux-saga/effects';
-import axios from 'axios';
-import { response } from 'express';
+import { put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+import { response } from "express";
 
 function* clientInfoSaga() {
-    yield takeLatest('GET_CLIENT_INFO', getClientInfo);
-
+  yield takeLatest("GET_CLIENT_DATA", getClientInfo);
 }
 
 function* getClientInfo(action) {
-    try {
-        const id = action.payload
-        yield axios.get(`/api/client/${id}`);
-        yield put({
-            type: 'SET_CLIENT_DATA',
-            payload: response.data,
-        });
-        console.log('here is data from client', response.data)
-    } catch (error) {
-        console.log('Error with get client info:', error);
-    }
+  try {
+    const id = action.payload;
+    yield axios.get(`/api/client/${id}`);
+    yield put({
+      type: "SET_CLIENT_DATA",
+      payload: response.data,
+    });
+    console.log("here is data from client", response.data);
+  } catch (error) {
+    console.log("Error with get client info:", error);
+  }
 }
 export default clientInfoSaga;

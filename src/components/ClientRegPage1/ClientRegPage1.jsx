2e03c67f8fdb517715c9e5bcd42c51a3f9b3f6b0
ThyChristiansen <thyvu0411@gmail.com>
2020-06-27 22:56:02 -0500

    sent img's url to server with uppy drag & drop

--- src/components/ClientRegPage1/ClientRegPage1.jsx
+++ src/components/ClientRegPage1/ClientRegPage1.jsx
@@ -145,48 +145,48 @@ class ClientRegPage1 extends Component {
     this.props.onNext();
   };
   //-----------------------------------
-  handlePictureChangeFor = (event) => {
-    console.log('changing', event.target.files[0])
+  // handlePictureChangeFor = (event) => {
+  //   console.log('changing', event.target.files[0])
 
-    this.setState({
-      file: event.target.files[0]
-    });
+  //   this.setState({
+  //     file: event.target.files[0]
+  //   });
+  // }
+
+  uppy = Uppy({
+    meta: { type: 'profilePicture' },
+    restrictions: { maxNumberOfFiles: 1 },
+    autoProceed: true
+  })
+
+  reader = new FileReader()
+
+  componentDidMount = () => {
+    this.uppy.on('upload', file => {
+      let fileKey = Object.keys(this.uppy.state.files)[0];
+      let fileFromUppy = this.uppy.state.files[fileKey].data;
+      this.setImage(fileFromUppy);
+    })
+
+    this.reader.onloadend = () => {
+      this.setState({
+        ...this.state,
+        profile_img: this.reader.result,
+      })
+    }
+    console.log('data from client reg page 1', this.state)
+
   }
 
-  // uppy = Uppy({
-  //   meta: { type: 'profilePicture' },
-  //   restrictions: { maxNumberOfFiles: 1 },
-  //   autoProceed: true
-  // })
-
-  // reader = new FileReader()
-
-  // componentDidMount = () => {
-  //   this.uppy.on('upload', file => {
-  //     let fileKey = Object.keys(this.uppy.state.files)[0];
-  //     let fileFromUppy = this.uppy.state.files[fileKey].data;
-  //     this.setImage(fileFromUppy);
-  //   })
-
-  //   this.reader.onloadend = () => {
-  //     this.setState({
-  //       ...this.state,
-  //       profile_img: this.reader.result,
-  //     })
-  //   }
-  //   console.log('data from client reg page 1', this.state)
-
-  // }
-
-  // setImage = file => {
-  //   //reads the file into a local data url
-  //   this.reader.readAsDataURL(file);
-  //   //sets the file into state and opens the walkthrough
-  //   this.setState({
-  //     ...this.state,
-  //     file: file,
-  //   })
-  // }
+  setImage = file => {
+    //reads the file into a local data url
+    this.reader.readAsDataURL(file);
+    //sets the file into state and opens the walkthrough
+    this.setState({
+      ...this.state,
+      file: file,
+    })
+  }
 
 
   //-----------------------------------
@@ -271,8 +271,8 @@ class ClientRegPage1 extends Component {
           >
             Select Photo to Upload
           </Button> */}
-          {/* <DragDrop uppy={this.uppy} /> */}
-          <input type="file" onChange={this.handlePictureChangeFor} />
+          <DragDrop uppy={this.uppy} />
+          {/* <input type="file" onChange={this.handlePictureChangeFor} /> */}
 
           <img className="upload-image-for-details" src={this.state.profile_img} alt="profilePictureUrl" width="100px" height="100px" />
 

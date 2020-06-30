bea6a352bbaae26066ce4b05654d9a692a784efd
Heather Stoffels <Umdstarlet@gmail.com>
2020-06-30 16:08:13 -0500

    added sendgrid, working and sending emails.

--- server/routes/request.router.js
+++ server/routes/request.router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const sgMail = require("@sendgrid/mail");
+const { getMaxListeners } = require('../modules/pool');
 
 /**
  * GET route template
@@ -25,7 +27,21 @@ router.get('/:id', (req, res) => {
  * POST route template
  */
 router.post('/', (req, res) => {
-    console.log("client request post::::", req.body, req.user.id);
+    console.log("client request post::::", req.body, req.user.id, req.user.user_email);
+    const hasEnvVariables =
+    process.env.SENDGRID_API_KEY && process.env.SENDGRID_FROM_EMAIL;
+  if (req.user.id && req.user.user_email && hasEnvVariables) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: req.user.user_email,
+      from: process.env.SENDGRID_FROM_EMAIL,
+      subject: "New service Request!",
+      text: " Test to send email",
+      html:
+        "Thanks for creating a new event to bring people together. More info to come before your event.",
+    };
+    sgMail.send(msg);
+  }
     const pet_id = parseInt(req.body.pet_id);
     const vet_id = req.user.id;
     const start_date_time = req.body.start_date_time;

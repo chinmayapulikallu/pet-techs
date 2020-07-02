e96685376629b59cb743421e93105bd8c16dbe04
Mike Cossalter <mikecossalter@yahoo.com>
2020-07-02 14:56:56 -0500

    about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,18 +1,80 @@
-import React from 'react';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
 
-const AboutPage = () => (
-  <div>
-    <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
-    </div>
-  </div>
-);
+const styles = (theme) => ({
+  landingPg: {
+    borderRadius: 0,
+    width: "100%",
+    height: "auto",
+    position: "relative",
+  },
+  description: {
+    textAlign: "center",
+    fontFamily: "Quicksand",
+    alignItems: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 400,
+    marginLeft: 400,
+    color: "#195C60",
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  image: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: 0,
+    paddingTop: 25,
+    width: 200,
+    height: 200,
+  },
+  missionHeader: {
+    textAlign: "center",
+    fontFamily: "Quicksand",
+    alignItems: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 400,
+    marginLeft: 400,
+    paddingTop: 100,
+    paddingBottom: 20,
+  },
+});
 
-export default AboutPage;
+class About extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+        <Grid>
+          <div>
+            <Typography className={classes.missionHeader} variant="h3">
+              Our Mission
+            </Typography>
+            <img
+              className={classes.image}
+              src="/images/mission-logo.png"
+              alt="missionLogoIcon"
+              height="300"
+              width="300"
+            />
+          </div>
+          <div>
+            <Typography className={classes.description} variant="h5">
+              We're here to connect vetinary technicians with pet owners who
+              want to hire qualified individuals to help give different kinds of
+              out-of-clinic care. Either in your home or theirs, you want to
+              know your precious animals are in great hands. We are commited to
+              giving quality and professional care and to give you ease of mind.{" "}
+            </Typography>
+          </div>
+          <Grid>
+          </Grid>
+        </Grid>
+    );
+  }
+}
+export default withStyles(styles)(About);

580c6012feb245b1449ea644ab710cb1c64bae34
Heather Stoffels <Umdstarlet@gmail.com>
2020-07-03 11:00:21 -0500

    clearing our reducers after vt registration.

--- src/redux/reducers/vetTechReducerPage3.js
+++ src/redux/reducers/vetTechReducerPage3.js
@@ -2,6 +2,8 @@ const vtInfoPage3 = (state = {}, action) => {
     switch (action.type) {
       case 'SET_VT_DATA_PAGE_3':
         return action.payload;
+        case "UNSET_VT_INFO":
+                return {};
       default:
         return state;
     }

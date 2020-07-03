ccb7f8175b374ff7597817f894f99417539f5886
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-07-03 16:22:44 -0500

    added email and end date for Vet dashboard

--- src/components/VTDashboard/VTDashboard.jsx
+++ src/components/VTDashboard/VTDashboard.jsx
@@ -86,8 +86,10 @@ class VTDashboard extends Component {
                     <TableHead>
                       <TableRow>
                         <TableCell>Name</TableCell>
-                        <TableCell>Date</TableCell>
+                        <TableCell>Service Start Date</TableCell>
+                        <TableCell>Service End Date</TableCell>
                         <TableCell>Client Name</TableCell>
+                         <TableCell>Client Email</TableCell>
                          <TableCell>Service Type</TableCell>
                         <TableCell>Details</TableCell>
                       </TableRow>
@@ -99,7 +101,9 @@ class VTDashboard extends Component {
                          <>
                         <TableCell>{request.pet_name}</TableCell>
                         <TableCell>{moment(request.start_date_time).format("MMM Do YYYY")}</TableCell>
+                        <TableCell>{moment(request.end_date_time).format("MMM Do YYYY")}</TableCell>
                         <TableCell>{request.client_name}</TableCell>
+                        <TableCell>{request.user_email}</TableCell>
                          <TableCell>{request.service_select}</TableCell>
                         <TableCell>
                           <Button
@@ -129,6 +133,8 @@ class VTDashboard extends Component {
                         <TableCell>Pet Name</TableCell>
                         <TableCell>Date</TableCell>
                         <TableCell>Species Name</TableCell>
+                        <TableCell>Client Name</TableCell>
+                        <TableCell>Client Email</TableCell>
                         <TableCell>Service Type</TableCell>
                         <TableCell>Action</TableCell>
                       </TableRow>
@@ -141,6 +147,8 @@ class VTDashboard extends Component {
                         <TableCell>{petRequest.pet_name}</TableCell>
                           <TableCell>{moment(petRequest.start_date_time).format("MMM Do YYYY")}</TableCell>
                         <TableCell>{petRequest.pet_type}</TableCell>
+                        <TableCell>{petRequest.client_name}</TableCell>
+                        <TableCell>{petRequest.user_email}</TableCell>
                         <TableCell>{petRequest.service_select}</TableCell>
                         <TableCell>
                           <Button

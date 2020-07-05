1b0ed617fa79477fe0b911520337b997bb5b9ef4
Mike Cossalter <mikecossalter@yahoo.com>
2020-07-05 11:21:12 -0500

    request posted, but sendgrid not sending

--- server/routes/request.router.js
+++ server/routes/request.router.js
@@ -39,47 +39,48 @@ router.get('/client', (req, res) => {
     .then((response) => {
       let petIdsForGivenUser = [...response.rows.map(pet => pet.id)]
       console.log(" PetIdsForGivenUser :: ", petIdsForGivenUser, req.user.id);
-        
+
       let sqlText = `select client_request.*, vet_tech.vet_name, pet.pet_name,
        (select client_name from client where client.user_id = $2) 
         from client_request, vet_tech, pet 
         where client_request.vet_id = vet_tech.user_id 
           and client_request.pet_id = pet.id 
           and client_request.pet_id = ANY($1::int[])`
-        pool
-          .query(sqlText, [petIdsForGivenUser, req.user.id])
-          .then((response) => {
-            console.log(" client service request:", response.rows);
-            //Since we only need the first, and only row, we are setting the index to 0.
-            res.send(response.rows);
-          })
-          .catch((error) => {
-            console.log(`Error in getting client service request. ${sqlText}`, error);
-            res.sendStatus(500);
-          });
-      })
+      pool
+        .query(sqlText, [petIdsForGivenUser, req.user.id])
+        .then((response) => {
+          console.log(" client service request:", response.rows);
+          //Since we only need the first, and only row, we are setting the index to 0.
+          res.send(response.rows);
+        })
+        .catch((error) => {
+          console.log(`Error in getting client service request. ${sqlText}`, error);
+          res.sendStatus(500);
+        });
+    })
     .catch((error) => {
-        console.log(`Error in getting getPetIdsForGivenUser ${getPetIdsForGivenUser}`, error);
-        res.sendStatus(500);
-    }); 
+      console.log(`Error in getting getPetIdsForGivenUser ${getPetIdsForGivenUser}`, error);
+      res.sendStatus(500);
+    });
 });
 
 /**
  * POST route template
  */
-router.post("/client", (req, res) => {
-    console.log("client request post::::", req.body, req.user.id, req.user.user_email);
-    const hasEnvVariables =
+router.post('/client', (req, res) => {
+  console.log("*****************************************************************************************************************", req.user.user_email)
+  console.log("client request post::::", req.body, req.user.id, req.user.user_email);
+  const hasEnvVariables =
     process.env.SENDGRID_API_KEY && process.env.SENDGRID_FROM_EMAIL;
-   if (req.user.id && req.user.user_email && hasEnvVariables) {
+  if (req.user.id && req.user.user_email && hasEnvVariables) {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
-      to: req.user.user_email,
+      to: "mikecossalter@yahoo.com",
       from: process.env.SENDGRID_FROM_EMAIL,
       subject: "New service Request!",
       text: " Test to send email",
       html:
-        "Thanks for creating a new event to bring people together. More info to come before your event.",
+        "You have a new service request from Pet Techs! Please login to your dashboard to view the request!",
     };
     sgMail.send(msg);
   }
@@ -89,7 +90,7 @@ router.post("/client", (req, res) => {
   //   const start_date_time = req.body.start_date_time;
   //   const end_date_time = req.body.end_date_time;
   //   const add_info = req.body.add_info;
-  
+
   //   // log.info("post client_request :: ", pet_id)
   //   // const request_status = req.body.request_status;
   //   const queryText =
@@ -111,37 +112,37 @@ router.post("/client", (req, res) => {
   //     res.sendStatus(500)
   //   });
 
-    const petIds = req.body.pet_id;
-    const vet_id = req.body.vet_id;
-    const start_date_time = req.body.start_date_time;
-    const end_date_time = req.body.end_date_time;
-    const add_info = req.body.add_info;
-    const input_info = req.body.input_info;
-    const service_select = req.body.service_select;
-  
-    // log.info("post client_request :: ", pet_id)
-    // const request_status = req.body.request_status;
+  const petIds = req.body.pet_id;
+  const vet_id = req.body.vet_id;
+  const start_date_time = req.body.start_date_time;
+  const end_date_time = req.body.end_date_time;
+  const add_info = req.body.add_info;
+  const input_info = req.body.input_info;
+  const service_select = req.body.service_select;
+
+  // log.info("post client_request :: ", pet_id)
+  // const request_status = req.body.request_status;
   const queryText =
     `INSERT INTO "client_request" ( pet_id, vet_id, start_date_time, end_date_time, add_info, input_info, service_select) 
       VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-    petIds.map(async petId => {
-      await pool
-    .query(queryText, [
-      petId,
-      vet_id,
-      start_date_time,
-      end_date_time,
-      add_info,
-      input_info,
-      service_select
-    ])
-    .then(() => res.sendStatus(201))
+  petIds.map(async petId => {
+    await pool
+      .query(queryText, [
+        petId,
+        vet_id,
+        start_date_time,
+        end_date_time,
+        add_info,
+        input_info,
+        service_select
+      ])
+      .then(() => res.sendStatus(201))
 
-    .catch((error) => {
-      console.log(error)
-      res.sendStatus(500)
-    });
-    })
+      .catch((error) => {
+        console.log(error)
+        res.sendStatus(500)
+      });
+  })
 });
 
 // router.post('/', (req, res) => {
@@ -166,17 +167,17 @@ router.post("/client", (req, res) => {
 //         add_info[i],
 //         // request_status
 //       ])
-     
+
 //       .then(() => res.sendStatus(201))
 //       .catch((error) => {
 //         console.log(error)
 //         res.sendStatus(500)
 //       });
 //   }
-  
 
 
-  
+
+
 
 // });
 
